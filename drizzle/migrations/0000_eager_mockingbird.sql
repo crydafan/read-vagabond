c586081fbbe8d61ad2cdc6b71bc8c7c7bf85ba3c
Leonardo <leonardo.estacio.h@gmail.com>
2026-05-28 00:16:29 -0500

    chore: regenerate migration

--- drizzle/migrations/0000_eager_mockingbird.sql
+++ drizzle/migrations/0000_eager_mockingbird.sql
@@ -6,15 +6,19 @@ CREATE TABLE `authors` (
 --> statement-breakpoint
 CREATE TABLE `chapters` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`manga_id` integer NOT NULL,
 	`volume_id` integer NOT NULL,
 	`title` text NOT NULL,
 	`number` integer NOT NULL,
 	`page_count` integer NOT NULL,
 	`release_date` integer,
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`manga_id`) REFERENCES `mangas`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`volume_id`) REFERENCES `volumes`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE INDEX `chapters_number_idx` ON `chapters` (`number`);--> statement-breakpoint
+CREATE UNIQUE INDEX `chapters_volume_id_number_unique` ON `chapters` (`volume_id`,`number`);--> statement-breakpoint
 CREATE TABLE `mangas` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`author_id` integer NOT NULL,
@@ -27,6 +31,7 @@ CREATE TABLE `mangas` (
 	FOREIGN KEY (`artist_id`) REFERENCES `authors`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE UNIQUE INDEX `mangas_author_id_title_unique` ON `mangas` (`author_id`,`title`);--> statement-breakpoint
 CREATE TABLE `volumes` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`manga_id` integer NOT NULL,
@@ -35,3 +40,5 @@ CREATE TABLE `volumes` (
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	FOREIGN KEY (`manga_id`) REFERENCES `mangas`(`id`) ON UPDATE no action ON DELETE no action
 );
+--> statement-breakpoint
+CREATE INDEX `volumes_number_idx` ON `volumes` (`number`);

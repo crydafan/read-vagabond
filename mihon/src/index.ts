0c34ef846772acbbecbaa202024124f6f77629fb
Leonardo <leonardo.estacio.h@gmail.com>
2026-06-09 12:22:03 -0500

    feat: update mihon api worker

--- mihon/src/index.ts
+++ mihon/src/index.ts
@@ -1,50 +1,66 @@
 import { Hono } from "hono";
+import { trimTrailingSlash } from "hono/trailing-slash";
+import { createDb, type Db } from "../../db";
 import {
   getMangaById,
   getMangaChapterByMangaIdAndChapterId,
   getMangaChaptersByMangaId,
   getMangas,
-} from "./lib/db";
-import { trimTrailingSlash } from "hono/trailing-slash";
+} from "../../db/queries";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono<{ Bindings: CloudflareBindings; Variables: { db: Db } }>();
 
 app.use(trimTrailingSlash());
 
+app.use(async (c, next) => {
+  c.set("db", createDb(c.env.bagabondo_db));
+  await next();
+});
+
 app.get("/mangas", async (c) => {
-  const mangas = await getMangas(c.env.bagabondo_db);
-  return c.json(mangas);
+  const mangas = await getMangas(c.var.db);
+  return c.json(
+    mangas.map((manga) => ({
+      ...manga,
+      status: "hiatus",
+      cover: "https://pub.moleve.net/covers/volume-37.jpg",
+    })),
+  );
 });
 
 app.get("/mangas/:mangaId", async (c) => {
   const { mangaId } = c.req.param();
-  const manga = await getMangaById(c.env.bagabondo_db, mangaId);
+  const manga = await getMangaById(c.var.db, Number(mangaId));
   if (!manga) {
     return c.text("Manga not found", 404);
   }
-  return c.json(manga);
+  return c.json({
+    ...manga,
+    status: "hiatus",
+    cover: "https://pub.moleve.net/covers/volume-37.jpg",
+  });
 });
 
 app.get("/mangas/:mangaId/chapters", async (c) => {
   const { mangaId } = c.req.param();
-  const manga = await getMangaChaptersByMangaId(c.env.bagabondo_db, mangaId);
-  if (!manga) {
+  const chapters = await getMangaChaptersByMangaId(c.var.db, Number(mangaId));
+  if (!chapters) {
     return c.text("Manga not found", 404);
   }
-  return c.json(manga);
+  return c.json(chapters);
 });
 
 app.get("/mangas/:mangaId/chapters/:chapterId", async (c) => {
   const { mangaId, chapterId } = c.req.param();
-  const chapters = await getMangaChapterByMangaIdAndChapterId(
-    c.env.bagabondo_db,
-    mangaId,
-    chapterId,
+  const chapter = await getMangaChapterByMangaIdAndChapterId(
+    c.var.db,
+    Number(mangaId),
+    Number(chapterId),
   );
-  if (!chapters) {
+  if (!chapter) {
     return c.text("Chapter not found", 404);
   }
-  return c.json(chapters);
+  return c.json(chapter);
 });
 
 export default app;
